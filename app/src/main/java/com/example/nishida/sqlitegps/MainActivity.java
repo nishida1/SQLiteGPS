815c64279c4b2b3a4e95d4932cc00c6302bad7cd
nishida1 <nishidanexus6@gmail.com>
2018-09-15 20:59:00 +0900

    permission

--- app/src/main/java/com/example/nishida/sqlitegps/MainActivity.java
+++ app/src/main/java/com/example/nishida/sqlitegps/MainActivity.java
@@ -2,21 +2,21 @@ package com.example.nishida.sqlitegps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
-    private String textLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textLog = "測位日時：2018-09-15 20:23:59\n緯　度　：123.456789\n経　度　：123.456789";
-        textView = (TextView) findViewById(R.id.text_view);
-        textView.setText(textLog);
-
+        locationPermission();
     }
+
+    // Intent で locationPermission
+    private void locationPermission() {
+        Intent intent = new Intent(getApplication(), LocationPermission.class);
+        startActivity(intent);
+    }
+
 }
